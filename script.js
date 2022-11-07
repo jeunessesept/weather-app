96d616653e432b74e39e824691981f9af62bc864
jeunessesept <b.jiacinto@gmail.com>
2022-11-07 14:48:38 +0100

    debut chart.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const inputedCity = document.querySelector(".citysearch");
 const buttons = document.querySelector(".searchbutton");
-const weatherContainer = document.querySelector(".weather");
-weatherContainer.style.visibility = "hidden";
+const grafCanvas = document.getElementById("chart");
+grafCanvas.style.display = "none";
+
+const humGraf = [];
+const dayGraf = [];
+
+
 
 const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-
 const fetchApiWeather = () => {
   let apiKey = "19e1717bb387f246a2c44f869eeb12ff";
-
-  weatherContainer.style.visibility = "visible";
 
   const fetchWeather = (city) =>
     fetch(
@@ -21,8 +23,18 @@ const fetchApiWeather = () => {
   fetchWeather(inCity)
     .then((response) => response.json())
     .then((json) => {
-      document.querySelector(".city").innerText =
-        "Weather in " + json.city.name;
+      
+      // dataGraf.push(json)
+
+      let weatherContainer = document.createElement("div");
+      weatherContainer.classList.add("weather");
+
+      let cityDiv = document.createElement("h2");
+      cityDiv.classList.add("city");
+      cityDiv.innerText = "Weather in " + json.city.name;
+
+      weatherContainer.append(cityDiv);
+      document.body.insertBefore(weatherContainer, grafCanvas);
 
       for (let i = 0; i < json.list.length; i = i + 8) {
         let elementDiv = document.createElement("div");
@@ -46,6 +58,12 @@ const fetchApiWeather = () => {
 
         elementDiv.append(daysDiv, iconDiv, tempDiv);
         weatherContainer.appendChild(elementDiv);
+        
+        humGraf.push(json.list[i].main.humidity)
+        dayGraf.push(json.list[i].dt_txt)
+       
+        
+    
       }
     })
     .catch((error) => {
@@ -55,30 +73,64 @@ const fetchApiWeather = () => {
 
 
 
-const fetchPicture =() => {
-   
-    
-   let city = inputedCity.value
-    fetch("https://api.unsplash.com/search/photos?&client_id=GZ5l_UJgVzdr3-GjchqGDOdlKHW0JonXPYGwpmhZ5T4&query=%22" + city )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json.results)
-        let picturesArray = json.results;
-        let item = picturesArray[Math.floor(Math.random()*picturesArray.length)]
-        console.log(item.urls.regular)
-        document.body.style.background = `url(${item.urls.regular}) no-repeat center`;
-        document.body.style.backgroundSize = "cover";
-        
-      });
-}
+const fetchPicture = () => {
+  let city = inputedCity.value;
+  fetch(
+    "https://api.unsplash.com/search/photos?&client_id=GZ5l_UJgVzdr3-GjchqGDOdlKHW0JonXPYGwpmhZ5T4&query=%22" +
+      city
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      let picturesArray = json.results;
+      let item =
+        picturesArray[Math.floor(Math.random() * picturesArray.length)];
+      document.body.style.background = `url(${item.urls.regular}) no-repeat center`;
+      document.body.style.backgroundSize = "cover";
+    });
+};
+
+
+
+const grafChart = () => {
+
+  // let date = new Date(dataGraf.dt_txt);
+
   
- 
+  const ctx = document.getElementById("chart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dayGraf,
+      datasets: [
+        {
+          label: "% of humidity",
+          data: humGraf,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+};
 
+////////calling 
 
-
-buttons.addEventListener("click", (event) => {
-  event.preventDefault();
+buttons.addEventListener("click", () => {
   fetchApiWeather();
   fetchPicture();
+  grafCanvas.style.display = "flex";
+  console.log(humGraf, dayGraf)
+  grafChart();
 });
 
+
+
+
+// function refreshPage() {
+//   window.location.reload();
+// }
